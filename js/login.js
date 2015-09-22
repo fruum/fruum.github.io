321f6a213a0d8fafaafa02221a1e2d32cc0080fc
Mr.Cockpit <bubblebeangrp+mrcockpit@googlemail.com>
2015-09-22 23:09:50 +0300

    Add social login popup

--- js/login.js
+++ js/login.js
@@ -1,23 +1,39 @@
+$(document).ready(function() {
+	hello.on('auth.login', function(auth) {
+		hello(auth.network).api('/me').then(function(r) {
+	    window.FruumData = window.FruumData || [];
+	    window.FruumData.push({
+	      user: {
+					network: auth.network,
+	        id: r.id,
+	        username: r.login || r.name,
+	        displayname: r.displayName || r.name,
+					email: r.email || '',
+	        avatar: r.thumbnail
+	      }
+	    });
+			$('#login').fadeOut('fast');
+			if (window.Fruum && window.Fruum.launch) {
+				window.Fruum.launch();
+			}
+		});
+	});
 
+	hello.init({
+		github: (window.location.host == 'fruum.github.io')?'7771e099483aa300bc02':'7d4f94583d9a8f453d59',
+		google: '298118064193-lsuue7safe22e0qgjgpv5vvkb8ublc31.apps.googleusercontent.com'
+	}, {redirect_uri: '/'});
 
-hello.on('auth.login', function(auth) {
-	hello(auth.network).api('/me').then(function(r) {
-    window.FruumData = window.FruumData || [];
-    window.FruumData.push({
-      user: {
-        id: r.id,
-        username: r.login,
-        displayname: r.name,
-        avatar: r.avatar_url
-      }
-    });
+	$('#login [data-auth]').click(function(event) {
+		event.preventDefault();
+		hello($(event.target).closest('[data-auth]').data('auth')).login({ scope: 'email' });
+	});
+	$('#login .popup-close').click(function(event) {
+		event.preventDefault();
+		$('#login').fadeOut('fast');
 	});
 });
 
-hello.init({
-	github: (window.location.host == 'fruum.github.io')?'7771e099483aa300bc02':'7d4f94583d9a8f453d59'
-}, {redirect_uri: '/'});
-
 function login() {
-  hello('github').login();
+	$('#login').fadeIn('fast');
 }
