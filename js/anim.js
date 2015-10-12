fdf6ea74265b1fa02a71b7ea2b75d8d254da63d5
Mr.Cockpit <bubblebeangrp+mrcockpit@googlemail.com>
2015-10-12 11:02:58 +0300

    Smoother animation

--- js/anim.js
+++ js/anim.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
       question = $('.explanation-fruumdot:first'),
       login = $('.explanation-login:first'),
       add = $('.explanation-add:first'),
+      close = $('.explanation-fruumclose'),
       loggedin = $('.explanation-loggedin:first');
 
   function position(el) {
@@ -16,6 +17,13 @@ $(document).ready(function() {
       left: el_offset.left - container_offset.left,
       top: el_offset.top - container_offset.top
     }
+  }
+
+  function click() {
+    marker.addClass('explanation-marker-clicked').delay(800);
+    setTimeout(function() {
+      marker.removeClass('explanation-marker-clicked');
+    }, 800);
   }
 
   //cursor moves to question mark
@@ -36,24 +44,26 @@ $(document).ready(function() {
 
   //fruum panel opens and user reads the content
   function step2() {
-    marker.addClass('explanation-marker-clicked');
+    click();
     fruum.addClass('explanation-fruumopen');
 
     //move marker inside fruum window
-    var fruum_position = position(fruum);
-    marker.delay(800).animate({
+    var fruum_position = position(fruum),
+        close_position = position(close);
+    marker.animate({
       left: fruum_position.left + fruum.width() / 2 + 'px',
       top: fruum_position.top + 20 + 'px'
-    }, 1000, function() {
+    }, 1500, function() {
       //scroll down marker
       marker.animate({
         top: fruum_position.top + fruum.height() * 0.75 + 'px'
-      }, 1000, function() {
+      }, 2000, function() {
         //Go to X and close window
-        marker.animate({
-          top: fruum_position.top + 20 + 'px',
-          left: fruum_position.left + fruum.width() + 'px'
-        }, 1000, function() {
+        marker.delay(800).animate({
+          top: close_position.top + close.height() / 2 + 'px',
+          left: close_position.left + close.width() / 2 + 'px'
+        }, 1500, function() {
+          click();
           fruum.removeClass('explanation-fruumopen');
           step3();
         });
@@ -64,12 +74,11 @@ $(document).ready(function() {
   //login
   function step3() {
     var login_position = position(login);
-    //scroll down marker
-    marker.delay(700).animate({
+    //goto login
+    marker.animate({
       top: login_position.top + login.height() /2 + 'px',
       left: login_position.left + login.width() /2 + 'px',
-    }, 1000, function() {
-      marker.addClass('explanation-marker-clicked');
+    }, 1500, function() {
       loggedin.css('opacity', 1);
       login.hide();
       step4();
@@ -78,13 +87,13 @@ $(document).ready(function() {
 
   //enter forum
   function step4() {
+    click();
     //go to question mark
     var question_pos = position(question);
     marker.animate({
       top: question_pos.top + question.height()/2 + 'px',
       left: question_pos.left + question.width()/2 + 'px'
     }, 2000, function() {
-      marker.addClass('explanation-marker-clicked');
       fruum_actions.show();
       fruum.addClass('explanation-fruumopen');
       step5();
@@ -93,21 +102,23 @@ $(document).ready(function() {
 
   //add thread
   function step5() {
+    click();
     var add_pos = position(add),
         fruum_pos = position(fruum);
     marker.animate({
       top: add_pos.top + add.height()/2 + 'px',
       left: add_pos.left + add.width()/2 + 'px'
     }, 2000, function() {
+      click();
       //show first message
       fruum_items.eq(0).delay(200).show();
       marker.animate({
         top: fruum_pos.top + fruum.height()/2 + 'px',
         left: fruum_pos.left + fruum.width()/2 + 'px'
       }, 2000, function() {
-        fruum_items.eq(1).delay(200).fadeIn(100);
+        fruum_items.eq(1).delay(500).fadeIn(100);
         //start over
-        setTimeout(step1, 2000);
+        setTimeout(step1, 2700);
       });
     });
   }
